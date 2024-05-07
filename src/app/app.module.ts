52b5f5e70edcc6ed9fc114ef67e99ce8687e0902
buhleqampi <buhle3857@gmail.com>
2024-05-07 07:36:33 +0200

    creation of recipe cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RecipeCardsComponent } from './recipe-cards/recipe-cards.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipeCardsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/recipe-cards/recipe-cards.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-cards',
+  templateUrl: './recipe-cards.component.html',
+  styleUrls: ['./recipe-cards.component.css']
+})
+export class RecipeCardsComponent {
+  @Input() recipes: any[] | undefined;
+
+
+  constructor() { }
+
+  showMoreDetails(recipe: any) {
+    // Implement showMoreDetails functionality here
+  
+  }
+}
+
