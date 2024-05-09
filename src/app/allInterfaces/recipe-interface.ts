784ba7a572df4cc794b05a7681ff9c2444642bd8
SivuyileMtwetwe <sivuyilemtwetwe@gmail.com>
2024-05-09 05:55:52 +0200

    btns

--- src/app/allInterfaces/recipe-interface.ts
+++ src/app/allInterfaces/recipe-interface.ts
@@ -10,5 +10,6 @@ export interface Recipe {
   servings: String;
   image: String;
   videoLink: String
+  voice: string
   }
   
--- src/app/recipe-details/recipe-details.component.ts
+++ src/app/recipe-details/recipe-details.component.ts
@@ -1,9 +1,8 @@
-import { Component, OnInit ,input} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { RecipeService } from '../allServices/recipe.service';
 import { Recipe } from '../allInterfaces/recipe-interface';
 import { Location } from '@angular/common';
-
 
 @Component({
   selector: 'app-recipe-details',
@@ -11,107 +10,97 @@ import { Location } from '@angular/common';
   styleUrls: ['./recipe-details.component.css']
 })
 export class RecipeDetailsComponent implements OnInit {
-  
-  recipe!: Recipe;
+  selectedRecipe?: Recipe;
 
-  constructor(private _activatedRoute: ActivatedRoute, private _recipeService: RecipeService, private _location: Location,
+  constructor(
+    private _activatedRoute: ActivatedRoute,
+    private _recipeService: RecipeService,
+    private _location: Location
   ) { }
 
-  
   ngOnInit(): void {
-this.getRecipe()
+    this.getRecipe();
   }
-
-  selectedRecipe?: Recipe
-
 
   getRecipe() {
     const recipeId = this._activatedRoute.snapshot.paramMap.get('id');
     this._recipeService.getAllRecipes().subscribe({
       next: (res) => {
-        const recipe = res.find((el: any) =>
-          el._id.includes(recipeId)
-        );
-
+        const recipe = res.find((el: any) => el._id.includes(recipeId));
         this.selectedRecipe = recipe;
-        console.log(this.selectedRecipe);
       },
-      error: console.log,
+      error: console.error
     });
   }
 
-playVideo(): void {
-  window.open("{{videoLink}}", "_blank");
+  goBack(){
+    this._location.back()
+  }
+
+  startTextToSpeech(): void {
+    const ingredients = (this.selectedRecipe?.ingredients || '').toString();
+    const instructions = (this.selectedRecipe?.instructions || '').toString();
+    mRead(ingredients, instructions);
+  }
   
 }
 
-goBack(): void {
-  this._location.back();
+let confirmationShown: boolean[] = [];
+
+function mRead(ingredients: string, instructions: string): void {
+
+
+  let utterance = new SpeechSynthesisUtterance(`Before we get you started, let's make sure you have the following ingredients: ${ingredients}`);
+  utterance.rate = 0.9;
+
+  const synth = window.speechSynthesis;
+  synth.cancel();
+  synth.onvoiceschanged = function () {
+    let voices = synth.getVoices();
+    utterance.voice = voices[2];
+
+    utterance.addEventListener("end", function () {
+      let utterance2 = new SpeechSynthesisUtterance(`Shall we begin?`);
+      utterance2.voice = voices[2];
+      utterance2.rate = 0.8;
+      synth.speak(utterance2);
+
+      if (!confirmationShown.includes(false)) {
+        const user = confirm("Shall we begin?");
+        if (user) {
+          confirmationShown = Array(instructions.split(".,").length).fill(false);
+          const utterance3 = new SpeechSynthesisUtterance(`Great! Let's begin. ${instructions}`);
+          utterance3.voice = voices[2];
+          utterance3.rate = 0.8;
+          synth.speak(utterance3);
+
+          const ins = instructions.split(".,");
+          let counter = 0;
+
+          const inter = setInterval(() => {
+            if (!confirmationShown[counter]) {
+              const utterance4 = new SpeechSynthesisUtterance(`Step ${counter + 1}, ${ins[counter]}`);
+              utterance4.voice = voices[2];
+              utterance4.rate = 0.8;
+              synth.speak(utterance4);
+              confirmationShown[counter] = true;
+              utterance4.addEventListener("end", function () {
+                confirm("Continue?");
+              });
+            }
+            counter++;
+            if (counter === ins.length) {
+              clearInterval(inter);
+              const utterance5 = new SpeechSynthesisUtterance(`Cooking complete. Well done!`);
+              utterance5.voice = voices[2];
+              utterance5.rate = 0.8;
+              synth.speak(utterance5);
+            }
+          }, 1500);
+        }
+      }
+    });
+
+    synth.speak(utterance);
+  };
 }
-
-// startTextToSpeech(): void {
-//   const ingredients = {{ingredients}};
-//   const instructions = {{instructions}};
-//   mRead(ingredients, instructions);
-// }
-}
-
-
-// let confirmationShown: boolean[] = [];
-
-// function mRead(ingredients: string, instructions: string): void {
-//   let utterance = new SpeechSynthesisUtterance(`Before we get you started, let's make sure you have the following ingredients: ${ingredients}`);
-//   utterance.rate = 0.9;
-
-//   const synth = window.speechSynthesis;
-//   synth.cancel();  
-//   synth.onvoiceschanged = function() {
-//     let voices = synth.getVoices();
-//     utterance.voice = voices[2];
-
-//     utterance.addEventListener("end", function() {
-//       let utterance2 = new SpeechSynthesisUtterance(`Shall we begin?`);
-//       utterance2.voice = voices[2];
-//       utterance2.rate = 0.8;
-//       synth.speak(utterance2);
-
-//       if (!confirmationShown.includes(false)) {
-//         const user = confirm("Shall we begin?");
-//         if (user) {
-//           confirmationShown = Array(instructions.split(".,").length).fill(false);
-//           const utterance3 = new SpeechSynthesisUtterance(`Great! Let's begin. ${instructions}`);
-//           utterance3.voice = voices[2];
-//           utterance3.rate = 0.8;
-//           synth.speak(utterance3);
-
-//           const ins = instructions.split(".,");
-//           let counter = 0;
-
-//           const inter = setInterval(() => {
-//             if (!confirmationShown[counter]) {
-//               const utterance4 = new SpeechSynthesisUtterance(`Step ${counter + 1}, ${ins[counter]}`);
-//               utterance4.voice = voices[2];
-//               utterance4.rate = 0.8;
-//               synth.speak(utterance4);
-//               confirmationShown[counter] = true;
-//               utterance4.addEventListener("end", function() {
-//                 confirm("Continue?");
-//               });
-//             }
-//             counter++;
-//             if (counter === ins.length) {
-//               clearInterval(inter);
-//               const utterance5 = new SpeechSynthesisUtterance(`Cooking complete. Well done!`);
-//               utterance5.voice = voices[2];
-//               utterance5.rate = 0.8;
-//               synth.speak(utterance5);
-//             }
-//           }, 1500);
-//         }
-//       }
-//     });
-
-//     synth.speak(utterance);
-//   };
-
-
